1240bca8fa668eff866eb041cce9cb77e94fc671
Ruben <ruben.cns@gmail.com>
2020-01-09 19:57:31 +0100

    Modify search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 const SearchBar = () => (
-
-    <div className="search-bar">
-        <div className="search-icon">
-            <i className="fas fa-search"></i>
+    <div className="search">
+        <div className="search-bar">
+            <div className="search-icon">
+                <i className="fas fa-search"></i>
+            </div>
+            <input type="text" className="search-input" placeholder="search for a movie..." />
         </div>
-        <input type="text" className="search-input" placeholder="search for a movie..." />
     </div>
 );
 
