575ec0d70e8d36ad2ef0de3c8a36744e4c570667
Ruben <ruben.cns@gmail.com>
2020-01-09 22:37:12 +0100

    Style movie component

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -15,10 +15,10 @@ const Movie = () => {
             </div>
             <div className="movie-item rate-fav">
                 <div className="movie-rating">
-                    <i className="fas fa-star"> 8.6</i>
+                    <i className="fas fa-star"> <span>8.6</span></i>
                 </div>
                 <button className="fav-btn">
-                    <i className="fas fa-heart"> Add to favorites</i>
+                    <i className="fas fa-heart"> <span>Add to favorites</span></i>
                 </button>
             </div>
             <p className="movie-item movie-plot">"In Gotham City, mentally troubled comedian Arthur Fleck is disregarded and mistreated by society. He then embarks on a downward spiral of revolution and bloody crime. This path brings him face-to-face with his alter-ego: the Joker."</p>
