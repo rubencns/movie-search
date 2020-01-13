78f88ca881ef17738ccac17d211247dc08f6acc1
Ruben <ruben.cns@gmail.com>
2020-01-13 20:08:37 +0100

    Show random movie recommendation when visiting the website for the first time

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import main from "../scss/main.scss";
+import { getMovie } from "./redux/actionCreators";
 import Routes from './Routes.jsx';
 import { Provider } from 'react-redux';
 import store from './redux/store.js';
 
+// Add random movie recommendations to avoid an empty space when visiting the website for the first time
+
+const movies = ["Inception", "Joker", "Interstellar", "Pulp Fiction",
+    "The Godfather", "Fight Club", "Lord Of The Rings", "Blade Runner"]
+
+const randomMovie = () => {
+    return movies[Math.floor(Math.random() * movies.length)]
+}
+
+store.dispatch(getMovie(randomMovie()));
 
 const App = () => (
 
