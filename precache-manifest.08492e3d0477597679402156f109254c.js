eec0f3f72f06d214258310eab553b62841780e45
Ruben <ruben.cns@gmail.com>
2020-01-14 18:43:58 +0100

    Updates

---
+++ precache-manifest.08492e3d0477597679402156f109254c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aedafbdde708895fdaa6d4c168d85464",
+    "url": "/movie-search/index.html"
+  },
+  {
+    "revision": "85dc6f2d320469c6c6e0",
+    "url": "/movie-search/static/css/main.588df8cd.chunk.css"
+  },
+  {
+    "revision": "d408aa5b05dbc8b8fdab",
+    "url": "/movie-search/static/js/2.8c078fe3.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/movie-search/static/js/2.8c078fe3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "85dc6f2d320469c6c6e0",
+    "url": "/movie-search/static/js/main.0a8054c0.chunk.js"
+  },
+  {
+    "revision": "19adb4a8c80add1a160d",
+    "url": "/movie-search/static/js/runtime-main.de947564.js"
+  }
+]);
